2c14267b61fd779acc7ec147853ba989bd3f309c
EdiDEngineer <33497530+EdiDEngineer@users.noreply.github.com>
2021-07-05 00:30:45 +0100

    Code path assignment 1

---
+++ Flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Edidiong Ekong on 04/07/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
